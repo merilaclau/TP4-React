c0f28c5159bc20969542a2df1f9acd78c61510c0
Meri Laclau <merilaclau@gmail.com>
2020-12-04 01:23:02 -0300

    folders + basic components structure + sass abstract files

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CardList.scss';
+
+function CardList() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default CardList;
---
+++ src/components/SearchInput/SearchInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchInput.scss';
+
+const SearchInput = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default SearchInput;
