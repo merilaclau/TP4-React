c6e92861e8ca592d52f59d743ff87fde4b4bd700
Maria Hayward <hayward.maria@gmail.com>
2020-12-08 16:25:34 -0300

    subo maquetado de home y nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './NavBar.scss';
+import SearchInput from '../SearchInput/SearchInput';
+import Menu from '../Menu/Menu';
 
 const NavBar = () => {
   return (
-    <div>
-      
-    </div>
-  );
+    <header className="navBar-container">
+      <section /* className="header-container" */>
+        <SearchInput />
+        <Menu />
+      </section>
+    </header>
+  )
 }
-
 export default NavBar;
