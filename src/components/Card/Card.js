c6c8f8c9faeff6e27d459e53aa197927aaa5e50d
AnabelCarranza <anabelcarranza@gmail.com>
2021-01-19 23:41:03 -0800

    img cardList

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Card.scss';
 
-const Card = () => {
+const Card = ({name, poster_path, rating}) => {
   return (
-    <div>
-      
-    </div>
+    <article className = "card">
+      <div className = "card--header">
+        <img className="card--img" src={poster_path} />   
+        <h3 className="card--header-title" >
+          {name}
+        </h3>  
+      </div>
+      <div className = "card--content">
+        <p>
+          {rating}
+        </p>
+      </div>  
+    </article>
+
   );
 }
 
